e78713d9efb8c477deb70c9077e2c087d3515fc9
Leo Cheng <leocheng@Leos-MacBook-Pro.local>
2024-06-24 14:53:23 -0400

    making sure we are able to work with inputs to parse_for_artifacts

--- parse_for_artifacts.py
+++ parse_for_artifacts.py
@@ -4,7 +4,7 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(description='Parses Json and passes arguments to download-artifacts-sh')
     parser.add_argument("-o", "--organization", default="nsls2-collection-tiled", help="The organization the "                                                                      "repository is under")
-    parser.add_argument("-a", "action_run", help="The ID(s) of the workflow that was run in array format")
+    parser.add_argument("-a", "--action_run", help="The ID(s) of the workflow that was run in array format")
     args = parser.parse_args()
     print(args.organization)
     print(args.action_run.split(' '))
