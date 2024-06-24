97ffcdde038de9fe74b8be0a0fcb77633b120f3c
Leo Cheng <leocheng@Leos-MacBook-Pro.local>
2024-06-24 15:06:30 -0400

    trying to access the artifact info from python scrip

--- parse_for_artifacts.py
+++ parse_for_artifacts.py
@@ -1,13 +1,24 @@
 import argparse
-
+import os
 
 def main():
     parser = argparse.ArgumentParser(description='Parses Json and passes arguments to download-artifacts-sh')
-    parser.add_argument("-o", "--organization", default="nsls2-collection-tiled", help="The organization the "                                                                      "repository is under")
+    parser.add_argument("-o", "--organization", default="nsls2-collection-tiled",
+                        help="The organization the repository is under")
     parser.add_argument("-a", "--action_run", help="The ID(s) of the workflow that was run in array format")
+    parser.add_argument("-f", "--file_name", default="artifact_info",
+                        help="jsonfile containg info about all artifacts created from some repository")
     args = parser.parse_args()
     print(args.organization)
-    print(args.action_run.split(' '))
+    action_ids = args.action_run.split(' ')
+    artifact_command = f'''
+    gh api \\
+            -H \"Accept: application/vnd.github+json\" \\
+            -H \"X-GitHub-Api-Version: 2022-11-28\" \\
+            /repos/nsls2-conda-envs/nsls2-collection-tiled/actions/artifacts >> {args.file_name}.json
+    '''
+    os.system(artifact_command)
+    os.system(f"cat {args.file_name}.json")
 
 if __name__ == "__main__":
     main()
