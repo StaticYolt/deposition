08fa8ea0aff45c397196849764a529447b091e32
Leo Cheng <leocheng@Leos-MacBook-Pro.local>
2024-06-24 15:28:37 -0400

    getting specific data from python script

--- parse_for_artifacts.py
+++ parse_for_artifacts.py
@@ -22,10 +22,12 @@ def main():
     for id in action_ids:
         for element in data['artifacts']:
             if element['workflow_run'].get('id') == int(id) and element['name'][-3:] != 'yml':
-                print("url: " + str(element['url']))
-                print("artifact_id: " + str(element['id']))
-                print("repo_name: " + str(str(element['url']).split('/')[5]))
-                print("env_name: " + str(element['name']))
+                os.system(f"echo \"link: {str(element['url'])}\"")
+                os.system(f"bash download-artifacts.sh {str(str(element['url']).split('/')[5])} {str(element['id'])} {str(element['name'])}")
+                # print("url: " + str(element['url']))
+                # print("artifact_id: " + str(element['id']))
+                # print("repo_name: " + str(str(element['url']).split('/')[5]))
+                # print("env_name: " + str(element['name']))
 
 if __name__ == "__main__":
     main()
